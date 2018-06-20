c55b415b0449b96bfda91e62307dd91146ec6471
edwinyoon <ep160101@gmail.com>
2018-06-19 22:37:32 -0400

    Edit route

--- frontend/src/routes/app.js
+++ frontend/src/routes/app.js
@@ -10,9 +10,7 @@ export default () => (
       <Header />
       <Switch>
         <Route exact path="/" component={AllCats} />
-        <Route exact path="/React" component={AllCats} />
-        <Route exact path="/Redux" component={AllCats} />
-        <Route exact path="/Saga" component={AllCats} />
+        <Route exact path="/:category" component={AllCats} />
       </Switch>
     </div>
   </Router>
