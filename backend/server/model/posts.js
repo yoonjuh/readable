a8ead740fd370222eb64ed6f1e2b2d3b43089b96
edwinyoon <ep160101@gmail.com>
2018-06-23 10:15:51 -0400

    Edit post schema

--- backend/server/model/posts.js
+++ backend/server/model/posts.js
@@ -1,18 +1,18 @@
-const { mongoose } = require('../../db/mongoose.js');
+const { mongoose } = require("../../db/mongoose.js");
 
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
   title: String,
+  body: String,
   author: String,
-  content: String,
-  voteScore: Number,
+  voteScore: { type: Number, default: 0 },
   date: { type: Date, default: Date.now },
-  comments: [{ commentTitle: String, body: String, date: Date }],
+  comments: [{ commentTitle: String, body: String, date: Date, default: {} }]
 });
 
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model("Post", postSchema);
 
 module.exports = {
-  Post,
+  Post
 };
