690c2a020a21b3f708eb917e90f2f67179e73fb4
edwinyoon <ep160101@gmail.com>
2018-06-27 23:06:00 -0400

    Initialize CSS

--- frontend/src/routes/app.js
+++ frontend/src/routes/app.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import Header from "../components/Header";
-import AllCats from "../components/AllCats";
+import Home from "../components/Home";
+import ByCategory from "../components/ByCategory";
+import PostForm from "../components/PostForm";
+import "./app.css";
 
 export default () => (
   <Router>
-    <div>
+    <div className="main-container">
       <Header />
       <Switch>
-        <Route exact path="/" component={AllCats} />
-        <Route exact path="/:category" component={AllCats} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/new" component={PostForm} />
+        <Route exact path="/:category" component={ByCategory} />
       </Switch>
     </div>
   </Router>
