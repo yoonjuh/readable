8d06d44f67391bb82a64b9e8881fe39a00a9f8b3
edwinyoon <ep160101@gmail.com>
2018-06-29 15:09:52 -0400

    Add initial styling with dummy data

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
-import Post frmm './Post'
+import styled from "styled-components";
+import Post from "./Post";
+import { data } from "./dummy";
 
 class Posts extends Component {
+  renderPosts = () => (
+    <div className="post-container">
+      {data.map(post => (
+        <ui>
+          <li>{post.title}</li>
+          <p>{post.body}</p>
+          <li>{post.author}</li>
+          <li>{post.date}</li>
+        </ui>
+      ))}
+    </div>
+  );
   render() {
-    return (
-    );
+    return <PostsContainer>{this.renderPosts()}</PostsContainer>;
   }
 }
 
 export default Posts;
+
+const PostsContainer = styled.div`
+  width: 100%;
+  height: 100%;
+
+  .post-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+  ui {
+    display: flex;
+    width: 30rem;
+    min-height: 20rem;
+    font-size: 2rem;
+    border: 0.1rem solid white;
+    list-style: none;
+  }
+`;
