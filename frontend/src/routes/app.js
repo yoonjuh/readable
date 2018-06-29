6589db0f775d1bee80815fd9a683825285770cd5
edwinyoon <ep160101@gmail.com>
2018-06-29 13:12:10 -0400

    Fix styling

--- frontend/src/routes/app.js
+++ frontend/src/routes/app.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "../components/Header";
-import Home from "../components/Home";
-import ByCategory from "../components/ByCategory";
 import PostForm from "../components/PostForm";
+import SortingToggler from "./SortingToggler";
 import "./app.css";
 
 export default () => (
-  <Router>
-    <div className="main-container">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/new" component={PostForm} />
-        <Route exact path="/:category" component={ByCategory} />
-      </Switch>
-    </div>
-  </Router>
-);
+    <Router>
+      <div className="main-container">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={SortingToggler} />
+          <Route exact path="/new" component={PostForm} />
+          <Route exact path="/:category" component={SortingToggler} />
+        </Switch>
+      </div>
+    </Router>
+  );
