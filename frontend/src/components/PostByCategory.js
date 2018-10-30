686eb03f16130048cf210bedfea199cd5b6e8b23
edwinyoon <ep160101@gmail.com>
2018-10-29 23:34:11 -0400

    Refactoring as a functional component

--- frontend/src/components/PostByCategory.js
+++ frontend/src/components/PostByCategory.js
@@ -1,55 +1,44 @@
-import React, { Component } from 'react'
-import Styled from 'styled-components'
-import PropTypes from 'prop-types'
-import gql from 'graphql-tag'
-import { uniq } from 'ramda'
-import { Query } from 'react-apollo'
-import CategoryNavigator from './CategoryNavigator'
-import NewPostButton from './NewPostButton'
-import Posts from './Posts'
+import React, { Component } from "react";
+import Styled from "styled-components";
+import PropTypes from "prop-types";
+import { compose, withState, withHandlers } from "recompose";
+import CategoryNavigator from "./CategoryNavigator";
+import NewPostButton from "./NewPostButton";
+import Posts from "./Posts";
 
-class PostByCategory extends Component {
-  state = {
-    currentCategory: '',
-  }
-  categoryToggler = newCategory => {
-    this.setState({ currentCategory: newCategory })
-  }
-
-  render() {
-    // console.log(this.props)
-    const { categories, posts } = this.props
-    return (
-      <MainContainer>
-        <NvaiContainer>
-          <CategoryNavigator
-            categories={uniq(categories.concat(['ALL'])).sort()}
-            currentCategory={this.state.currentCategory}
-            categoryToggler={this.categoryToggler}
-          />
-        </NvaiContainer>
-        <NewPostButton />
-        <Posts {...this.props} posts={posts} />
-      </MainContainer>
-    )
-  }
-}
-export default PostByCategory
-// props => (
-//   <Query query={GET_ALL_POST}>{({ data, loading }) => <PostByCategory {...props} data={data} />}</Query>
-// )
+const EnhancedPostByCategory = compose(
+  withState("currentCat", "setCategory", ""),
+  withHandlers({
+    categoryToggler: ({ setCategory }) => newCat => {
+      setCategory(newCat);
+    }
+  })
+)(({ currentCat, categoryToggler, categories = [], posts = [] }) => (
+  <MainContainer>
+    <NvaiContainer>
+      <CategoryNavigator
+        categories={categories}
+        currentCat={currentCat}
+        categoryToggler={categoryToggler}
+      />
+    </NvaiContainer>
+    <NewPostButton />
+    <Posts {...this.props} posts={posts} />
+  </MainContainer>
+));
+export default EnhancedPostByCategory;
 
 const MainContainer = Styled.div`
   max-width: 90%;
   max-height: 100%;
   display: flex;
   flex-direction: column;
-`
+`;
 
 const NvaiContainer = Styled.div`
   flex: 1;
-`
+`;
 
-PostByCategory.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.string).isRequired,
-}
+EnhancedPostByCategory.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.string).isRequired
+};
