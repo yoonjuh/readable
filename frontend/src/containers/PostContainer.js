5a98285294d227b74fc4cae2e7c5d132786b749c
edwinyoon <ep160101@gmail.com>
2018-11-02 12:10:16 -0400

    Modify project structure

---
+++ frontend/src/containers/PostContainer.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import Styled from "styled-components";
+import PropTypes from "prop-types";
+import { compose, withState, withHandlers, withStateHandlers } from "recompose";
+import { isEmpty } from "ramda";
+import CategoryNavigator from "../components/CategoryNavigator";
+import NewPostButton from "../components/NewPostButton";
+import Posts from "../components/Posts";
+
+const PostContainer = ({ categoryToggler, categories, posts }, ...props) => {
+  const [currentCat, setCurrentCat] = useState("all");
+  const [sortedPosts, sortPost] = useState([]);
+  useEffect(
+    () => {
+      if (currentCat === "all") {
+        const newPosts = posts.filter(post => post.category === currentCat);
+        sortPost(newPosts);
+      } else {
+        const newPosts = posts.filter(post => post.category === currentCat);
+        sortPost(newPosts);
+      }
+    },
+    [currentCat]
+  );
+
+  return (
+    <MainContainer>
+      <NvaiContainer>
+        <CategoryNavigator
+          categories={categories}
+          currentCat={currentCat}
+          categoryToggler={setCurrentCat}
+        />
+      </NvaiContainer>
+      <NewPostButton />
+      <Posts posts={isEmpty(sortedPosts) ? posts : sortedPosts} />
+    </MainContainer>
+  );
+};
+export default PostContainer;
+
+const MainContainer = Styled.div`
+  max-width: 90%;
+  max-height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const NvaiContainer = Styled.div`
+  flex: 1;
+`;
+
+PostContainer.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.string).isRequired
+};
